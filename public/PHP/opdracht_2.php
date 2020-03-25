c85cb0f92b7547203443f9c9f16657ce2309dea2
Esmee-W <150532@csgaugustinus.nl>
2020-03-25 11:17:27 +0000

    opdracht 2

--- public/PHP/opdracht_2.php
+++ public/PHP/opdracht_2.php
@@ -7,8 +7,16 @@ TYP HIERONDER JOUW PHPCODE
 
 $tekst="Ik heb een bijbaantje bij de Aldi. De Aldi betaalt goed.";
 echo "$tekst";
-//$tekst=str_replace('Aldi','Albert Heijn',$tekst);
-echo '<h4>$tekst</h4>';
+$tekst=str_replace('Aldi','Albert Heijn',$tekst);
+$tekst=strstr($tekst,"De");
+$woorden=str_word_count($tekst);
+echo '<h4>'.$tekst.'</h4>';
+$lengte=strlen($tekst);
+echo '<p><i>De string $tekst bestaat uit <b>'.$lengte.'</b> karakters.</i></p>';
+echo '<p><i>De string $tekst bestaat uit <b>'.$woorden.'</b> woorden.</i></p>';
+$tekst=substr($tekst,10,5);
+echo '<p>'.$tekst.'</p>';
+echo strrev($tekst).'<br>';
 
 /****************************
 EINDE VAN JOUW PHPCODE
