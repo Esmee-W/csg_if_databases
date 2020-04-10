a62cddcc866b706f005d6b28e38e2b3eedfb64b4
Esmee-W <150532@csgaugustinus.nl>
2020-04-10 07:08:33 +0000

    Opdracht 9 (niet 50)

--- public/PHP/opdracht_9.php
+++ public/PHP/opdracht_9.php
@@ -11,17 +11,17 @@ for ($o=1; $o<=6; $o++) {
 
 function dobbelsteen() {
   $worp=1+rand(0,5);
-  echo $worp." | ";
+  //echo $worp." | ";
   return $worp;
 }
 
-for ($n=0; $n<20; $n++) {
+for ($n=0; $n<3000; $n++) {
   $frequentie[dobbelsteen()]++;  
 }
 
 echo '<h2>Staafdiagram frequentie</h2>';
 for ($o=1; $o<=6; $o++) {
-  $hoogte=$frequentie[$o]*50;
+  $hoogte=$frequentie[$o]*0.5;
   echo '<img src="images/FF4D00-0.8.png" style="width: 100px; height: '.$hoogte.'px; margin-left: 5px;">';  
 }
 
