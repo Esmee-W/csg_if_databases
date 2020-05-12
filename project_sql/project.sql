c09ed1df7d330e5784ad6405b05d3fdbf9aa4511
Anniek-W <150136@csgaugustinus.nl>
2020-05-12 10:07:41 +0000

    data

--- project_sql/project.sql
+++ project_sql/project.sql
@@ -29,7 +29,7 @@ CREATE TABLE `producten` (
   `type` text NOT NULL,
   `smaak` text NOT NULL,
   `soort` text NOT NULL,
-  `gewicht` int(11) NOT NULL,
+  `gewicht` decimal(5,1) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -40,7 +40,7 @@ CREATE TABLE `producten` (
 
 LOCK TABLES `producten` WRITE;
 /*!40000 ALTER TABLE `producten` DISABLE KEYS */;
-INSERT INTO `producten` VALUES (1,'Chocoladehagel puur',2.17,'Strooibaar','Pure chocolade','Hagelslag',380),(2,'Chocoladehagel melk',2.17,'Strooibaar','Melk chocolade','Hagelslag',380),(3,'Witte vlokken',1.78,'Strooibaar','Witte chocolade','Vlokken',300),(4,'Vlokken puur',1.77,'Strooibaar','Pure chocolade','Vlokken',300),(5,'Vlokken melk',1.77,'Strooibaar','Melk chocolade','Vlokken',300),(6,'Vlokken vlok feest',1.79,'Strooibaar','Mix','Vlokken',300),(7,'Royaal vlokken',1.85,'Strooibaar','Extra pure chocolade','Vlokken',300),(8,'Royaal hagel',2.09,'Strooibaar','Extra pure chocolade','Hagelslag',380),(9,'Specials koffie - puur',1.97,'Strooibaar','Mix','Hagelslag',220),(10,'Specials extra puur',1.97,'Strooibaar','Extra pure chocolade','Hagelslag',240),(11,'Specials intens puur',1.77,'Strooibaar','Intens pure chocolade','Hagelslag',220),(12,'Chunks extra puur',2.19,'Strooibaar','Extra pure chocolade','Chunks',200),(13,'Chunks melk',2.19,'Strooibaar','Melk chocolade','Chunks',200),(14,'Vruchtenhagel bosvruchten',1.38,'Strooibaar','Bosvruchten','Hagelslag',300),(15,'Vruchtenhagel',1.38,'Strooibaar','Vruchten','Hagelslag',400);
+INSERT INTO `producten` VALUES (1,'Chocoladehagel puur',2.17,'Strooibaar','Pure chocolade','Hagelslag',380.0),(2,'Chocoladehagel melk',2.17,'Strooibaar','Melk chocolade','Hagelslag',380.0),(3,'Witte vlokken',1.78,'Strooibaar','Witte chocolade','Vlokken',300.0),(4,'Vlokken puur',1.77,'Strooibaar','Pure chocolade','Vlokken',300.0),(5,'Vlokken melk',1.77,'Strooibaar','Melk chocolade','Vlokken',300.0),(6,'Vlokken vlok feest',1.79,'Strooibaar','Mix','Vlokken',300.0),(7,'Royaal vlokken',1.85,'Strooibaar','Extra pure chocolade','Vlokken',300.0),(8,'Royaal hagel',2.09,'Strooibaar','Extra pure chocolade','Hagelslag',380.0),(9,'Specials koffie - puur',1.97,'Strooibaar','Mix','Hagelslag',220.0),(10,'Specials extra puur',1.97,'Strooibaar','Extra pure chocolade','Hagelslag',240.0),(11,'Specials intens puur',1.77,'Strooibaar','Intens pure chocolade','Hagelslag',220.0),(12,'Chunks extra puur',2.19,'Strooibaar','Extra pure chocolade','Chunks',200.0),(13,'Chunks melk',2.19,'Strooibaar','Melk chocolade','Chunks',200.0),(14,'Vruchtenhagel bosvruchten',1.38,'Strooibaar','Bosvruchten','Hagelslag',300.0),(15,'Vruchtenhagel',1.38,'Strooibaar','Vruchten','Hagelslag',400.0),(16,'Rose en witte muisjes®',1.67,'Strooibaar','Anijs','Muisjes',280.0),(17,'Blauwe en witte muisjes®',1.67,'Strooibaar','Anijs','Muisjes',280.0),(18,'Gestampte muisjes®',1.18,'Strooibaar','Anijs','Muisjes',230.0),(19,'Anijshagel',1.19,'Strooibaar','Anijs','Hagelslag',300.0),(20,'Anijsstaafjes',1.45,'Drinkbaar','Anijs','Staafjes',75.6),(21,'Kleintjes strooibaar',1.97,'Strooibaar','Mix','Kleintjes',140.0),(22,'Kleintjes extra jam',1.97,'Smeerbaar','Mix','Kleintjes',300.0),(23,'Kleintjes smeerbaar',1.87,'Smeerbaar','Mix','Kleintjes',120.0);
 /*!40000 ALTER TABLE `producten` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-05-12  9:46:29
+-- Dump completed on 2020-05-12 10:07:33
