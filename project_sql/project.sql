05973b00db30231e81257d83369ccd550918a0c3
Anniek-W <150136@csgaugustinus.nl>
2020-05-12 09:07:30 +0000

    data

--- project_sql/project.sql
+++ project_sql/project.sql
@@ -14,6 +14,35 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `producten`
+--
+
+DROP TABLE IF EXISTS `producten`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `producten` (
+  `id` int(11) NOT NULL,
+  `naam` text NOT NULL,
+  `prijs` decimal(5,2) NOT NULL,
+  `type` text NOT NULL,
+  `smaak` text NOT NULL,
+  `soort` text NOT NULL,
+  `gewicht` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `producten`
+--
+
+LOCK TABLES `producten` WRITE;
+/*!40000 ALTER TABLE `producten` DISABLE KEYS */;
+INSERT INTO `producten` VALUES (1,'Chocoladehagel puur',2.17,'Strooibaar','Puur','Hagelslag',380),(2,'Chocoladehagel melk',2.17,'Strooibaar','Melk','Hagelslag',0),(3,'Witte vlokken',1.78,'Strooibaar','Witte chocolade','Vlokken',0);
+/*!40000 ALTER TABLE `producten` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -24,4 +53,4 @@
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-03-19 14:02:21
+-- Dump completed on 2020-05-12  9:06:45
