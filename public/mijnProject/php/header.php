e51991c6daf8266a8b053eae9d8e7f29723a4508
Esmee-W <150532@csgaugustinus.nl>
2020-06-04 13:53:28 +0000

    naam

--- public/mijnProject/php/header.php
+++ public/mijnProject/php/header.php
@@ -8,7 +8,7 @@
 
     <body>
         <div id="rectangletop"></div>
-        <a href="https://8001-f01c6303-c285-4b58-a909-119c895cd5d2.ws-eu01.gitpod.io/mijnProject/php/filter.php">
+        <a href="https://8001-c2f9d18d-4db5-43cf-9087-0d996ff269c2.ws-eu01.gitpod.io/mijnProject/">
             <img class="fotologotop" src="images/logo-de-ruijter.png">
         </a>
         <button class="button button1"onclick="location.href='https://8001-c2f9d18d-4db5-43cf-9087-0d996ff269c2.ws-eu01.gitpod.io/mijnProject/';"><b>Home</b></button>
