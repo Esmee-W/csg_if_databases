453cf1270a0f527c3e3e0f9f31e8f994c7095e5b
Anniek-W <150136@csgaugustinus.nl>
2020-06-04 13:36:34 +0000

    filter

---
+++ public/mijnProject/php/VNRdata.php
@@ -0,0 +1,23 @@
+<?php
+$sqlToevoeging = $_GET["q"];
+$sql = "SELECT * FROM producten".$sqlToevoeging;
+
+require('database.php');
+$database = "project";
+$DBverbinding = mysqli_connect($servernaam, $gebruikersnaam, $wachtwoord, $database);
+
+
+echo "<h2>RESULTAAT</h2>";
+echo "<h5>$sql</h5>";
+
+$records = mysqli_query($DBverbinding, $sql);
+if (mysqli_num_rows($records) > 0) {  
+  while($record = mysqli_fetch_assoc($records)) {
+    echo "<b>".$record["naam"]."</b><br>";
+  }
+}
+else {
+  echo "0 resultaten";
+}
+mysqli_close($DBverbinding); 
+?> 
