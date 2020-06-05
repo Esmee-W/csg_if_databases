287ddc0115929d8784167e4bbb0c82c4ca944bb3
Esmee-W <150532@csgaugustinus.nl>
2020-06-05 07:03:36 +0000

    footer af

---
+++ public/mijnProject/over.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Over deze website</title>
+        <link rel="stylesheet" type="text/css" href="css/design.css">
+    </head>
+    <body>
+        <?php include 'php/header.php';?>
+        <div id="container">
+            <h1>
+                Over deze website
+            </h1>
+        </div>
+        <?php include 'php/footer.php';?>
+    </body>
+</html>
