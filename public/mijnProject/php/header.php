893721652c9c59fe6f22f1856d74fa308549ca00
Esmee-W <150532@csgaugustinus.nl>
2020-06-22 11:22:49 +0000

    knoppen

--- public/mijnProject/php/header.php
+++ public/mijnProject/php/header.php
@@ -8,12 +8,12 @@
 
     <body>
         <div id="rectangletop"></div>
-        <a href="https://8001-e7ec6180-aed3-4b16-b3ea-01d8189684b9.ws-eu01.gitpod.io/mijnProject/">
+        <a href="/mijnProject/">
             <img class="fotologotop" src="images/logo-de-ruijter.png">
         </a>
-        <button class="buttontop button1"onclick="location.href='https://8001-e7ec6180-aed3-4b16-b3ea-01d8189684b9.ws-eu01.gitpod.io/mijnProject/';"><b>Home</b></button>
-        <button class="buttontop button2"onclick="location.href='https://8001-e7ec6180-aed3-4b16-b3ea-01d8189684b9.ws-eu01.gitpod.io/mijnProject/filter.php';"><b>Producten</b></button>
-        <button class="buttontop button3"onclick="location.href='https://8001-e7ec6180-aed3-4b16-b3ea-01d8189684b9.ws-eu01.gitpod.io/mijnProject/contact.php';"><b>Contact</b></button>
-        <button class="buttontop button4"onclick="location.href='https://8001-e7ec6180-aed3-4b16-b3ea-01d8189684b9.ws-eu01.gitpod.io/mijnProject/over.php';"><b>Over</b></button>
+        <button class="buttontop button1"onclick="location.href='/mijnProject';"><b>Home</b></button>
+        <button class="buttontop button2"onclick="location.href='/mijnProject/filter.php';"><b>Producten</b></button>
+        <button class="buttontop button3"onclick="location.href='/mijnProject/contact.php';"><b>Contact</b></button>
+        <button class="buttontop button4"onclick="location.href='/mijnProject/over.php';"><b>Over</b></button>
     </body>
 </html>
